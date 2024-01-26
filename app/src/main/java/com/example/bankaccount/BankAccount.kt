29a634a3319d2d6136270115d465d8b3120bb250
danifdezalonso <37313326+danifdezalonso@users.noreply.github.com>
2024-01-26 13:25:02 +0100

    done

--- app/src/main/java/com/example/bankaccount/BankAccount.kt
+++ app/src/main/java/com/example/bankaccount/BankAccount.kt
@@ -1,4 +1,35 @@
 package com.example.bankaccount
 
-class BankAccount {
+class BankAccount(
+    var accountHolder:String,
+    var balance:Double
+    )
+{
+    private val transactionHistory = mutableListOf<String>()
+    fun deposit(amount: Double){
+        balance += amount
+        transactionHistory.add("$accountHolder ha depositado $amount€")
+    }
+
+    fun withdraw(amount: Double){
+        if(amount <= balance){
+            balance -=amount
+            transactionHistory.add("$accountHolder ha retirado $amount€")
+
+            println("Aquí tienes tus $amount€, recuerda recoger tu tarjeta, tonto")
+        }else{
+            println("No tienes suficiente dinero, pobre")
+        }
+    }
+
+    fun acctBalance(): Double{
+        return balance
+    }
+
+    fun displayTransactionHistory(){
+        println("historial de transacciones de $accountHolder")
+        for (transaction in transactionHistory){
+            println(transaction)
+        }
+    }
 }
--- app/src/main/java/com/example/bankaccount/BankMain.kt
+++ app/src/main/java/com/example/bankaccount/BankMain.kt
@@ -1,5 +1,18 @@
 package com.example.bankaccount
 
 fun main(){
+    val daniBankAccount = BankAccount("Dani Fernandez", 4.0 )
+    daniBankAccount.deposit(50.0)
+    daniBankAccount.deposit(20.0)
+    daniBankAccount.withdraw(8.0)
+    daniBankAccount.deposit(10.0)
+    daniBankAccount.displayTransactionHistory()
+    println("Tienes ${daniBankAccount.acctBalance()}€")
+
+    val sarahBankAccount = BankAccount("Sarah", 5.0)
+    sarahBankAccount.deposit(100.0)
+    sarahBankAccount.deposit(50.0)
+    sarahBankAccount.withdraw(8.0)
+    println("Tienes ${sarahBankAccount.acctBalance()}€")
 
 }
